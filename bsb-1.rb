30188c0a76f5116b152ee187bec1f94bab119345
Niko Escobal <niko.escobal@gmail.com>
2020-12-11 04:47:13 +0800

    Add bubble_sort_by method.

--- bsb-1.rb
+++ bsb-1.rb
@@ -1,39 +1,38 @@
 # Now create a similar method called #bubble_sort_by which sorts an array by accepting a block. Remember to use yield inside your method definition to accomplish this. The block will have two arguments that represent the two elements of the array that are currently being compared. The block’s return will be similar to the spaceship operator you learned about before: If the result of the block execution is negative, the element on the left is “smaller” than the element on the right. 0 means both elements are equal. A positive result means the left element is greater. Use the block’s return value to sort your array. Test your method with the example below.
-#   > bubble_sort_by(["hi","hello","hey"]) do |left,right|
-#   >   left.length - right.length
-#   > end
-#   => ["hi", "hey", "hello"] 
 
-def bubble_sort_by(&ah)
-return "shit" unless block_given? 
-ah.call 6, 2
+# attempt #1
+
+def bubble_sort_by(arr)
+yield "hello"
 end
 
-# bubble_sort_by do |y, z|
-#   puts y - z
-#   puts y * 10, z * 2
-# end 
-
-# lol_poo = -> {puts "lol"}
-
-
-# lol_poo.call
-
-# t = Proc.new { |x,y| puts "I don't care about arguments!" }
-# t.call
-
-# # Should work
-# my_lambda = -> { return 1 }
-# puts "Lambda result: #{my_lambda.call}"
-# # Should raise exception
-# my_proc = Proc.new { return 1 }
-# puts "Proc result: #{my_proc.call}"
-
-def call_proc
-  puts "Before proc"
-  my_proc = Proc.new { return 2 }
-  my_proc.call
-  puts "After proc"
+bubble_sort_by(array) do |y,z|
+y.length - z.length
 end
-p call_proc
-# Prints "Before proc" but not "After proc"
+
+array = ["hi","hello","hey"]
+
+attempt #2
+
+def bubble_sort_by
+  while y < self.length
+    if self[y] < self[z]
+   self[y], self[z] = self[z], self[y]
+    end
+  end
+yield
+end
+
+["hi","hello","hey"].bubble_sort_by do |y,z|
+y.length - z.length
+end
+
+[3,5].bubble_sort_by { |y, z| puts y }
+
+
+
+
+
+
+
+
